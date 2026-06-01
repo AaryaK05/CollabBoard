d498c4eca18dbb1698a3c3b2c574e1fda9494381
AaryaDesktop <aaryakasbe05@gmail.com>
2026-06-02 01:03:17 +0530

    Added a loading screen to improve UI

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,41 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {io} from 'socket.io-client';
 import Home from './pages/Home';
 import Main from './pages/Main';
+import { useEffect, useState } from 'react';
+import Loader from '../src/components/Loader'
 
 const socket=io('https://whiteboard-collab-ucab.onrender.com');
+const Url="https://whiteboard-collab-ucab.onrender.com";
 
 function App() {
+  const [isServerReady,setIsServerReady]=useState(false);
+  const [isCheckingServer,setIsCheckingServer]=useState(true);
+
+  function checkServerStatus(){
+    fetch(Url).then((res)=>{
+      if(res.ok){
+        setIsServerReady(true);
+      }
+      else{
+        console.log("Error",res.status);
+        alert(res.status);
+      }
+    }).catch((err)=>{
+      console.log("Server not ready!");
+      alert(err);
+    }).finally(()=>{
+      setIsCheckingServer(false);
+    })
+  }
+
+  useEffect(()=>{
+    checkServerStatus();
+  },[20])
+
+  if(isCheckingServer || !isServerReady){
+    return <Loader/>
+  }
+
   return (
     <BrowserRouter>
       <Routes>
---
+++ client/src/components/Loader.js
@@ -0,0 +1,11 @@
+
+function Loader(){
+    return(
+        <div className="loader-body">
+            <div className="loader"></div>
+            <div>Waiting for server to load...</div>
+        </div>
+    )
+}
+
+export default Loader;
